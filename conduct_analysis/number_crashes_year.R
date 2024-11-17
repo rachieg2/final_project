cf12e6e5fe9c034d7652f66b6df34a7c80035b9c
Rachel Kudzin <heyrachel221@gmail.com>
2024-11-17 00:23:25 +0000

    Add y lim at 0

--- conduct_analysis/number_crashes_year.R
+++ conduct_analysis/number_crashes_year.R
@@ -10,6 +10,7 @@ num_crashes_year <- ggplot(counts_by_year, aes(x = year, y = n)) +
     geom_point() +
     geom_smooth() +
     xlab("Year") +
-    ylab("Number of Plane Crashes")
+    ylab("Number of Plane Crashes") +
+    scale_y_continuous(expand = c(0, 0), limits = c(0, 90))
 
 ggsave("figures/num_crashes_by_year.png", num_crashes_year)
