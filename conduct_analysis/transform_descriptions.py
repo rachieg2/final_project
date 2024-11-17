1735614b5fe3fd18c1cdd007273d5d5ad21c1789
Rachel Kudzin <heyrachel221@gmail.com>
2024-11-17 01:09:43 +0000

    Save the plane desc vectors in the cleaned data folder

--- conduct_analysis/transform_descriptions.py
+++ conduct_analysis/transform_descriptions.py
@@ -63,7 +63,7 @@ def get_word_counts_for_crash(plane_data: pd.DataFrame) -> dict:
 
 
 def get_total_word_count(summary_word_counts: dict) -> dict:
-    """Calculates the total word count across all episodes.
+    """Calculates the total word count across all plane descriptions.
 
     Args:
         summary_word_counts (dict): A dictionary object of crashes and descriptions.
@@ -79,32 +79,50 @@ def get_total_word_count(summary_word_counts: dict) -> dict:
     return total_word_count
 
 
-def convert_to_word_count_vectors(episode_word_counts, filtered_words):
-    """Converts word counts for each episode into a vector following the filtered word order."""
+def convert_to_word_count_vectors(
+    summary_word_counts: dict, filtered_words: list
+) -> dict:
+    """Converts word counts for each description into a vector following the filtered word order.
+
+    Args:
+        summary_word_counts (dict): Dictionary of word counts from plane descriptions
+        filtered_words (list): List of words of interest
+
+    Returns:
+        dict: The same summary_word_count dictionary, with words in filtered_words order
+    """
     word_vectors = {}
 
-    for url, word_counts in episode_word_counts.items():
+    for idx, word_counts in summary_word_counts.items():
         # Create a vector for this episode by the order of filtered_words
         vector = [word_counts.get(word, 0) for word in filtered_words]
-        word_vectors[url] = vector
+        word_vectors[idx] = vector
 
     return word_vectors
 
 
 def write_word_counts_to_csv(
-    word_count_vectors, filtered_words, filename="episode_word_counts.csv"
+    word_count_vectors: dict,
+    filtered_words: list,
+    filename: str = "cleaned_data/plane_description_counts.csv",
 ):
-    """Writes the episode word count vectors to a CSV file."""
+    """Writes the plane crash description word count vectors to a CSV file.
+
+    Args:
+        word_count_vectors (dict): Plane crash descriptions as vectors.
+        filtered_words (list): List of all words (filtered).
+        filename (str, optional): Path where to save the data. Defaults to "cleaned_data/plane_description_counts.csv".
+    """
     with open(filename, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
 
-        # Write the header row (episode URL and each word)
-        header = ["Episode URL"] + filtered_words
+        # Write the header row (crash number and each word)
+        header = ["Crash_NUM"] + filtered_words
         writer.writerow(header)
 
-        # Write each episode's word count vector
-        for url, vector in word_count_vectors.items():
-            row = [url] + vector
+        # Write each plane crash's word count vector
+        for idx, vector in word_count_vectors.items():
+            row = [idx] + vector
             writer.writerow(row)
 
 
@@ -121,12 +139,10 @@ if __name__ == "__main__":
     # Filter words with a total count greater than 20 and sort by frequency
     filtered_words = [word for word, count in total_word_count.items() if count > 20]
 
-    # Convert each episode's word counts into a vector of word counts
+    # Convert each descriptions's word counts into a vector of word counts
     word_count_vectors = convert_to_word_count_vectors(
-        episode_word_counts, filtered_words
+        plane_desc_counts, filtered_words
     )
 
     # Write the word count vectors to a CSV file
     write_word_counts_to_csv(word_count_vectors, filtered_words)
-
-    print("Word counts written to 'episode_word_counts.csv'")
